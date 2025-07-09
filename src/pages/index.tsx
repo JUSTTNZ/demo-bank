6cb0cc517ce30ffce371467d3d746ee23f9c5db6
JUSTTNZ <chukwudiconfidenceosinachi@gmail.com>
2025-07-09 20:39:44 +0100

    Update background color of HomePage to use primary theme color

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
   return (
-    <main className="min-h-screen flex items-center justify-center bg-red-900">
-      <h1 className="text-3xl font-bold">Welcome to Demo Bank 🏦</h1>
+    <main className="min-h-screen flex items-center justify-center bg-primary">
+      <h1 className="text-3xl text-secondary font-bold">Welcome to Demo Bank 🏦</h1>
     </main>
   )
 }
