360cd11d6bb4d8c91a0c923ed6f9b309387a43ff
JUSTTNZ <chukwudiconfidenceosinachi@gmail.com>
2025-07-15 04:38:53 +0100

    done

--- src/pages/user.tsx
+++ src/pages/user.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import UserDashboard from '@/components/users/userDashboard'
+import UserDashboard from '@/components/userDashboard'
 
 export default function AdminPage() {
   return (
