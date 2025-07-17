fe1e93a0fda02fd46b835d3e7df955c9fbde0e8d
JUSTTNZ <chukwudiconfidenceosinachi@gmail.com>
2025-07-17 05:41:40 +0100

    done

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  images: {
+    
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'bptmlpuyxhcwjzwfbctt.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
